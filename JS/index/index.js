92efef7f3b4c44d53ce2de8c91c4b3ede7bdf128
SeeChen Lee <leeseechen@gmail.com>
2023-03-12 12:19:12 +0800

    Update index.js

--- JS/index/index.js
+++ JS/index/index.js
@@ -12,6 +12,7 @@ function loadPageLanguage() {
 	$.getJSON(languageUrl, function(data) {
 		
 		let languageObj = data["zh"][0];
+		console.log(languageObj);
 		
 		$("title:eq(0)").text(languageObj.title);
 	});
