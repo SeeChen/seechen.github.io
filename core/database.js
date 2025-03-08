f31db8d2765a75ed096c909d9aacecd91ef1c48c
SeeChen <leeseechen@gmail.com>
2025-03-08 14:05:39 +0800

    [Issue https://github.com/SeeChen/seechen.github.io/issues/35] Update Core/Database

--- core/database.js
+++ core/database.js
@@ -10,53 +10,204 @@
 */
 
 class BaseDatabase {
+    static #ConfigSchema = {
+        warn    : { type: "boolean" , default: true },
+        dir     : { type: "string"  , default: null },
+        type    : { type: "string"  , default: null }
+    };
 
-    #DatabaseDirectory = null;
-    #DatabaseType      = null;
+    #Config = Object.keys(BaseDatabase.#ConfigSchema).reduce((acc, key) => {
+        acc[key] = BaseDatabase.#ConfigSchema[key].default;
+        return acc;
+    }, {});
 
-    constructor (
-        path,
+    static status = {
+        collection: {
+            notfound: Symbol("not-found"),
+            new     : Symbol("new"),
+            loading : Symbol("loading"),
+            loaded  : Symbol("loaded")
+        }
+    };
+    #Status = new Map();
+
+    constructor(
+        path, 
         type
     ) {
-
         if (typeof path !== "string") {
-            throw new TypeError(`Database Directory must be a string, your path is ${typeof(path)}`);
+            throw new TypeError(`Database Directory must be a string, but got ${typeof path}.`);
+        }
+        if (typeof type !== "string") {
+            throw new TypeError(`Database type must be a string, but got ${typeof type}.`);
         }
 
-        this.#DatabaseType      = type.toLowerCase();
-        this.#DatabaseDirectory = path;
+        this.#Config["dir"]     = path;
+        this.#Config["type"]    = type.toLowerCase()
     }
 
-    getConfig () {
-        
+    get config() {
         return {
-            type:       this.#DatabaseType,
-            directory:  this.#DatabaseDirectory
+            set: (
+                newConfig
+            ) => {
+                Object.entries(newConfig).forEach(([key, value]) => {
+                    if (!this.#Config.hasOwnProperty(key)) {
+                        throw new ReferenceError(`Invalid config key: "${key}". Allowed keys: [${Object.keys(this.#Config).join(", ")}]`);
+                    }
+                    const ExpectedType = BaseDatabase.#ConfigSchema[key].type;
+                    if (typeof value != ExpectedType) {
+                        throw new TypeError(`Invalid type for key "${key}". Expected <${ExpectedType}>, but got <${typeof value}>.`);
+                    }
+                    this.#Config[key] = value;
+                });
+            },
+    
+            get: (
+                key = null
+            ) => {
+                if (!key) {
+                    return {...this.#Config};
+                }
+                if (!BaseDatabase.#ConfigSchema.hasOwnProperty(key)) {
+                    throw new ReferenceError(`Invalid config key: "${key}".`);
+                }
+                return this.#Config[key];
+            }
+        }
+    }
+
+    get status() {
+        return {
+            set: (
+                ...NewStatus
+            ) => {
+                let LengthStatus = NewStatus.length;
+                if (LengthStatus % 2 != 0) {
+                    throw new RangeError("Invalid number of arguments: Must be an even number of key-value pairs.");
+                }
+                for (let i = 0; i < NewStatus.length; i += 2) {
+                    const key = NewStatus[i];
+                    const value = NewStatus[i + 1];
+            
+                    if (typeof key !== "string" || typeof value !== "symbol") {
+                        throw new TypeError(`Expected <string, symbol> pairs, but got <${typeof key}, ${typeof value}>.`);
+                    }
+            
+                    this.#Status.set(key, value)
+                }
+            },
+
+            get: () => {
+                return {
+                    byName: (
+                        CollectionName
+                    ) => {
+                        return this.#Status.get(CollectionName) || BaseDatabase.status.collection.notfound;
+                    },
+
+                    byStatus: (
+                        CollectionStatus
+                    ) => {
+                        if (typeof CollectionStatus !== "symbol") {
+                            throw new TypeError("Expected a Symbol, but got " + typeof CollectionStatus);
+                        }
+                    
+                        if (!Object.values(BaseDatabase.status.collection).includes(CollectionStatus)) {
+                            throw new ReferenceError("Invalid collection status");
+                        }
+
+                        return [...this.#Status.entries()]
+                                .filter(([, v]) => v === CollectionStatus)
+                                .map(([k]) => k);
+                    }
+                }
+            }
         }
     }
 }
 
 class databaseJson extends BaseDatabase {
-    
+
+    #DatabaseCollection = new Set();
+
+    get collection() {
+        return {
+            set: (
+                ListCollection,
+                ExistOK = true
+            ) => {
+                if (!Array.isArray(ListCollection)) {
+                    throw new TypeError(`This function only accepts an array. Your input type: ${typeof ListCollection}`);
+                }
+        
+                ListCollection.forEach(collection => {
+                    if (this.#DatabaseCollection.has(collection)) {
+                        if (!ExistOK) {
+                            throw new Error(`The collection "${collection}" already exists!`);
+                        }
+                        if (super.config.get("warn")) {
+                            console.warn(`"${collection}" already exists.`);
+                        }
+                    } else {
+                        this.#DatabaseCollection.add(collection);
+                        super.status.set(collection, BaseDatabase.status.collection.new);
+                    }
+                });
+            },
+
+            get: (
+                CollectionName = null
+            ) => {
+                if (!CollectionName) {
+                    return [...this.#DatabaseCollection].map(key => ({
+                        name: key,
+                        path: `${super.config.get("dir")}/${key}.${super.config.get("type")}`,
+                        status: super.status.get().byName(key)
+                    }));
+                }
+
+                if (typeof CollectionName === "string") {
+                    return {
+                        name: CollectionName,
+                        path: `${super.config.get("dir")}/${CollectionName}.${super.config.get("type")}`,
+                        status: super.status.get().byName(CollectionName)
+                    }
+                }
+                
+                if (Array.isArray(CollectionName)) {
+                    return CollectionName.map(name => this.collection.get(name));
+                }
+
+                throw new TypeError(`Invalid argument: CollectionName must be <string> or <array>. Received value: "${CollectionName}" (type: <${typeof CollectionName}>)`);
+            }
+        }
+    }
+
+    get document() {
+        return {
+
+        }
+    }
 }
 
 export const Database = {
-
     create: (
-        path,
+        path, 
         type
     ) => {
-
-        const listType = {
-            json: databaseJson,
-        };
-
-        type = type.toLowerCase();
-        if (!listType[type]) {
-
-            throw new TypeError(`Currently is not supported Type: ${type}`);
+        if (typeof type !== "string") {
+            throw new TypeError(`Type must be a string, but got <${typeof type}>.`);
         }
 
-        return new listType[type](path, type);
+        const listType = Object.freeze({
+            json: databaseJson
+        });
+
+        const dbClass = listType[type.toLowerCase()];
+        if (!dbClass) {
+            throw new TypeError(`Currently unsupported Type: ${type}`);
+        }
+        return new dbClass(path, type);
     }
-}
+};
--- main.js
+++ main.js
@@ -4,5 +4,26 @@ import { Database as db } from "/core/database.js";
 window.onload = function () {
 
     const jsonDatabase = db.create("test", "JSON");
-    console.log(jsonDatabase.getData());
+    console.log(jsonDatabase)
+
+    jsonDatabase.collection.set(
+        ["a"]
+    )
+
+    jsonDatabase.config.set({warn: true});
+    jsonDatabase.collection.set(
+        ["b"]
+    )
+
+    console.log(jsonDatabase);
+
+    console.log(jsonDatabase.status.get().byName("a"))
+
+
+    console.log("New Test");
+    console.log(jsonDatabase.collection.get(["a", "b"]))
+    console.log(jsonDatabase.collection.get("abc"))
+    console.log(jsonDatabase.collection.get({
+        1: "a"
+    }))
 }
